dce1a8e95699f5307aa712654872e2a7b73babec
Wang Wei <thegenius@vip.qq.com>
2017-05-17 11:17:30 +0800

    add person test

--- src/test/java/com/lvonce/logicweaver/examples/PersonTest.java
+++ src/test/java/com/lvonce/logicweaver/examples/PersonTest.java
@@ -1,18 +1,26 @@
 package com.lvonce.logicweaver.examples;
 
+import com.lvonce.logicweaver.BehaviorDebug;
+import com.lvonce.logicweaver.executors.BehaviorExecutor;
+import com.lvonce.logicweaver.interfaces.IBehaviorNode;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class PersonTest {
     @Test
     public void test() {
-//        IBehaviorEntity entity = new Person();
-//        IBehaviorNode logic = PersonLogic.getLogic();
-//        if (logic != null){
-//            BehaviorExecutor executor = new BehaviorExecutor(entity, logic);
-//            ArrayList<Integer> commandList = executor.runDebug();
-//            BehaviorDebug.debug("command list:%s", commandList.toString());
-//            commandList = executor.runDebug();
-//            BehaviorDebug.debug("command list:%s", commandList.toString());
-//        }
+        Person entity = new Person();
+        IBehaviorNode logic = PersonLogic.logic;
+        if (logic != null){
+            BehaviorExecutor executor = new BehaviorExecutor(entity, logic);
+            ArrayList<Integer> commandList = executor.runDebug();
+            assertArrayEquals(new Integer[]{1, 2, 4, 5}, commandList.toArray(new Integer[4]));
+
+            commandList = executor.runDebug();
+            assertArrayEquals(new Integer[]{6, 6}, commandList.toArray(new Integer[2]));
+        }
     }
 }
